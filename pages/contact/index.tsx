2cd38109522aea3c8474e3f7f89d1e11c73df2c4
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-04-02 11:32:28 -0300

    yarn buikd and yarn start to deploy intranet

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { MainLayout } from '../../components/layouts/MainLayout'
-import { Navbar } from '../../components/Navbar.tsx'
+import { Navbar } from '../../components/Navbar'
 
 export default function ContactPage() {
   return (
